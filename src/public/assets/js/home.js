bf16dddd714c8b561625e27e5c50ff174ffab64c
María José Oliveira <proyectos.joseoliveira@gmail.com>
2024-09-11 19:04:07 -0300

    carts

--- src/public/assets/js/home.js
+++ src/public/assets/js/home.js
@@ -4,41 +4,51 @@ let user;
 const addToCartButtons = document.querySelectorAll('.addToCart')
 const verCarrito = document.querySelector('#verCarrito')
 
-Swal.fire({
-    title: '¡Identifícate!',
-    input: 'text',
-    text: 'Ingrese un nombre de usuario para poder acceder a nuestro sitio.',
-    inputValidator: (value) => {
-        return !value && 'Necesitas escribir un nombre de usuario para conectarte';
-    },
-    allowOutsideClick: false // Para que no se cierre si hago click fuera del alert.
-}).then(result => {
-    if (result.isConfirmed) { // Verifica que el usuario confirme el diálogo
-        user = result.value;
-        console.log('Nombre de usuario ingresado:', user);
+window.onload = function () {
+    const username = localStorage.getItem('username')
 
-        fetch('/carts', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
+    if (!username) {
+        Swal.fire({
+            title: '¡Identifícate!',
+            input: 'text',
+            text: 'Ingrese un nombre de usuario para poder acceder a nuestro sitio.',
+            inputValidator: (value) => {
+                return !value && 'Necesitas escribir un nombre de usuario para conectarte';
             },
-            body: JSON.stringify({ user: user })
-        })
-            .then(response => {
-                if (!response.ok) { // Verifica que la respuesta sea exitosa
-                    throw new Error('Error en la respuesta del servidor');
-                }
-                return response.json();
-            })
-            .then(data => {
-                localStorage.setItem('cartId', data.postCart._id)
-                console.log('Cart ID guardado en el localStorage:', data.postCart._id);
-            })
-            .catch(error => {
-                console.error('Error al enviar el nombre de usuario:', error);
-            });
+            allowOutsideClick: false // Para que no se cierre si hago click fuera del alert.
+        }).then(result => {
+            if (result.isConfirmed) { // Verifica que el usuario confirme el diálogo
+                user = result.value;
+                console.log('Nombre de usuario ingresado:', user);
+                localStorage.setItem('username', user)
+
+                fetch('/carts', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ user: user })
+                })
+                    .then(response => {
+                        if (!response.ok) { // Verifica que la respuesta sea exitosa
+                            throw new Error('Error en la respuesta del servidor');
+                        }
+                        return response.json();
+                    })
+                    .then(data => {
+                        localStorage.setItem('cartId', data.postCart._id)
+                        console.log('Cart ID guardado en el localStorage:', data.postCart._id);
+                    })
+                    .catch(error => {
+                        console.error('Error al enviar el nombre de usuario:', error);
+                    });
+            }
+        });
+
     }
-});
+}
+
+
 
 
 addToCartButtons.forEach(button => {
